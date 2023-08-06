809245ae7595c799ccae321f90fb77ecb3194990
micogongob <15572400+micogongob@users.noreply.github.com>
2023-08-06 18:13:23 +0800

    Update app logging

--- single/src/main/scala/com/example/app/Application.scala
+++ single/src/main/scala/com/example/app/Application.scala
@@ -6,7 +6,9 @@ object Application {
 
   def main(args: Array[String]): Unit = {
     val helper = new ScalaProjectHelper()
+    println("Starting ...")
     println(s"App info: ${helper.getAppInfo()}")
+    println("DONE")
   }
 
 }
