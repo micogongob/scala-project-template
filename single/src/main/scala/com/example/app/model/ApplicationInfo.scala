9b3e147e1feeffbeb4b105c262b001e56f3261af
micogongob <15572400+micogongob@users.noreply.github.com>
2023-08-06 17:27:39 +0800

    Refactor model setup

--- single/src/main/scala/com/example/app/model/ApplicationInfo.scala
+++ single/src/main/scala/com/example/app/model/ApplicationInfo.scala
@@ -5,3 +5,9 @@ class ApplicationInfo(val name: String, val version: String) {
     return s"ApplicationInfo(name=$name,version=$version)"
   }
 }
+
+object ApplicationInfo {
+  def apply(name: String, version: String): ApplicationInfo = {
+    return new ApplicationInfo(name, version)
+  }
+}
--- single/src/main/scala/com/example/app/utils/ScalaProjectHelper.scala
+++ single/src/main/scala/com/example/app/utils/ScalaProjectHelper.scala
@@ -4,6 +4,6 @@ import com.example.app.model.ApplicationInfo
 
 class ScalaProjectHelper {
   def getAppInfo(): ApplicationInfo = {
-    return new ApplicationInfo("scala-project-template", "1.0.0")
+    return ApplicationInfo("scala-project-template", "1.0.0")
   }
 }
