972a04fb61d95d71c74227e79d9e5eb1da59234b
micogongob <15572400+micogongob@users.noreply.github.com>
2024-05-02 14:46:51 +0800

    Use case class

--- single/src/main/scala/com/example/template/model/Context.scala
+++ single/src/main/scala/com/example/template/model/Context.scala
@@ -1,13 +1,8 @@
 package com.example.template.model
 
-class Context(val greeting: String, val target: String) {
+case class Context(val greeting: String, val target: String) {
   override def toString(): String = {
     return s"Context(greeting=$greeting,target=$target)"
   }
 }
 
-object Context {
-  def apply(greeting: String, target: String): Context = {
-    return new Context(greeting, target)
-  }
-}
